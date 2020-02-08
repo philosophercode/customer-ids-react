b582d6feb13180ce0de136ccd9eadfab959819c1
Isaac S <steinbergisaac@outlook.com>
2020-02-07 22:59:33 -0800

    test deploy 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import List from "./components/List";
+import List from "./List";
 
 function App() {
   return (
     <div className="App">
-      TEST
         <List />
     </div>
   );
